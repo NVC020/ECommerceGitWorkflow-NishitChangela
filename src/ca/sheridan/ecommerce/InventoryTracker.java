1e47835a1adadf7d4073a37bbb05134483ad7b04
NVC020 <changeln@sheridancollege.ca>
2026-02-19 18:05:16 -0500

    Implemented stock checking by Nishit Changela

--- src/ca/sheridan/ecommerce/InventoryTracker.java
+++ src/ca/sheridan/ecommerce/InventoryTracker.java
@@ -2,9 +2,15 @@ package ca.sheridan.ecommerce;
 
 public class InventoryTracker {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        System.out.println("Inventory Tracker started...");
+    }
 
-	}
+    // Method 1: Check stock level
+    public void checkStockLevel(String productId, int currentStock) {
+        System.out.println("Checking stock for product: " + productId);
+        System.out.println("Current stock: " + currentStock);
+    }
 
-}
+   
+    }
